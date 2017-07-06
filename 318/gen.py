e617eb91f50177a06d087f88ea114478cd445d3a
luolei <luoleicn@gmail.com>
2017-07-06 15:53:36 +0000

    reorder 318

---
+++ 318/gen.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#coding=utf8
+
+header = """---
+layout: github
+title: 川藏线骑行多图慎入
+---
+
+# 川藏线骑行照片
+
+点击图片跳转到下一页
+"""
+
+for i in range(2, 42):
+    content ="[![image](http://www.luolei.info/source/images/318-{}.jpg)](http://www.luolei.info/318/318-{})".format(i, i + 1)
+    writer = open("318-" + str(i) + ".md", "w")
+    writer.write(header + "\n" + content)
+    writer.close()
+
